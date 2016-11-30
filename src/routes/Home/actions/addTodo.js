7fc87394f78185d8c754dd1f091e2264a871d536
Joseph Nam <josephknam@gmail.com>
2016-11-29 19:06:19 -0500

    some small

--- src/routes/Home/actions/addTodo.js
+++ src/routes/Home/actions/addTodo.js
@@ -56,9 +56,8 @@ const fetchAddTodo = (todo) => {
         console.log('gapi is', window.gapi)
         if (todo.addToGCalendar === true) {
           const event = {
-            'summary': 'Google I/O 2015',
-            'location': '800 Howard St., San Francisco, CA 94103',
-            'description': 'A chance to hear more about Google\'s developer products.',
+            'summary': todo.name,
+            'description': todo.description,
             'start': {
               'dateTime': '2016-11-29T09:00:00-07:00',
               'timeZone': 'America/Los_Angeles'
@@ -66,20 +65,6 @@ const fetchAddTodo = (todo) => {
             'end': {
               'dateTime': '2016-11-29T17:00:00-07:00',
               'timeZone': 'America/Los_Angeles'
-            },
-            'recurrence': [
-              'RRULE:FREQ=DAILY;COUNT=2'
-            ],
-            'attendees': [
-              { 'email': 'lpage@example.com' },
-              { 'email': 'sbrin@example.com' }
-            ],
-            'reminders': {
-              'useDefault': false,
-              'overrides': [
-                { 'method': 'email', 'minutes': 24 * 60 },
-                { 'method': 'popup', 'minutes': 10 }
-              ]
             }
           }
           window.gapi.client.load('calendar', 'v3', () => {
